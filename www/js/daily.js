4ba0f8304e67b5da4ff934f52299b5d5efc58d7c
Ben Hill <ben@neocom.co.nz>
2014-12-10 10:57:47 +1300

    1

--- www/js/daily.js
+++ www/js/daily.js
@@ -1,3 +1,26 @@
-/**
- * Created by benh on 12/10/2014.
- */
+var db;
+
+document.addEventListener("deviceready", onDeviceReadydaily, false);
+
+function onDeviceReadydaily() {
+    db.transaction(getdata, errorCBfunc, successCBfunc);
+}
+
+function getdata(tx) {
+    var sql = "select ID ,CreatedateUTC ,UpdatedateUTC ,DeletedateUTC ,BusinessID ,BusinessLocationID,StartDate ,EndDate ,ItemName,Details ,Price ,URL from MobilevwApp_weeklydeal ";
+    // alert(sql);
+    tx.executeSql(sql, [], getdata_success);
+}
+
+function getdata_success(tx, results) {
+    $('#busy').hide();
+    var len = results.rows.length;
+
+    for (var i=0; i<len; i++) {
+        var menu = results.rows.item(i);
+        alert(menu.ItemName);
+
+
+    }
+
+}
