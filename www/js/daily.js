45eb97c9197d082b70945d664536e01fb3d59462
Ben Hill <ben@neocom.co.nz>
2014-12-15 11:00:27 +1300

    1

--- www/js/daily.js
+++ www/js/daily.js
@@ -7,7 +7,7 @@ function onDeviceReadydaily() {
 }
 
 function getdata(tx) {
-    var sql = "select ID ,CreatedateUTC ,UpdatedateUTC ,DeletedateUTC ,BusinessID ,BusinessLocationID,StartDate ,EndDate ,ItemName,Details ,Price ,URL from MobilevwApp_dailydeal ";
+    var sql = "select MAD.ID as ID,MAD.StartDate as StartDate ,MAD.EndDate as EndDate ,MAD.ItemName as ItemName,MAD.Details as Details ,MAD.Price as Price ,MAD.URL as URL, MBN.Icon as Icon from MobilevwApp_dailydeal as MAD JOIN MobileApp_BusinessNames as MBN on MAD.BusniessID = MBN.ID ";
     // alert(sql);
     tx.executeSql(sql, [], getdata_success);
 }
@@ -18,7 +18,19 @@ function getdata_success(tx, results) {
 
     for (var i=0; i<len; i++) {
         var menu = results.rows.item(i);
-        alert(menu.ItemName);
+        var imgg = "";
+        if(menu.Icon != "null"){
+            imgg = '&nbsp;<img src="data:image/png;base64,' + menu.Icon + '"  align="center" width="200" >&nbsp;';
+            //imgg = menu.BusinessName;
+        }else{
+
+            imgg = menu.BusinessName;
+        }
+
+        $('#Categoriesbus').append('<Div align="center"  class="modal-body"  style="border-bottom: 1px solid #e5e5e5;"    >' +
+        '<div align="center"  >' + imgg +
+        '</div>' +
+        '</Div>');
     }
 
 }
