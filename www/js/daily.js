9a031724572a79b4d960bc155c595a30da3837c8
Ben Hill <ben@neocom.co.nz>
2014-12-15 12:21:20 +1300

    1

--- www/js/daily.js
+++ www/js/daily.js
@@ -22,6 +22,8 @@ function getdata_success(tx, results) {
     for (var i=0; i<len; i++) {
         var menu = results.rows.item(i);
         var res = (menu.EndDate).split("T");
+        alert(res[0]);
+        alert(res[1]);
         var split = res[0].split("-");
         var month = split[1];
         var year = split[0];
