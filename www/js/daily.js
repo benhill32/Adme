cff51cced47865e7eaa041e5356d9cb43d014243
Ben Hill <ben@neocom.co.nz>
2014-12-15 11:38:17 +1300

    1

--- www/js/daily.js
+++ www/js/daily.js
@@ -34,9 +34,9 @@ function getdata_success(tx, results) {
 
 
 
-        '<div align="center" class="floatleft3"  >time</div>' +
-        '<div align="center"  class="floatleft3"  >' + imgg + '</div>' +
-        '<div align="center"  class="floatleft3"  >Read more</div>' +
+        '<div align="center" class="floatleft3 padding200"  >time</div>' +
+        '<div align="center"  class="floatleft3 padding200"  >' + imgg + '</div>' +
+        '<div align="center"  class="floatleft3 padding200"  >Read more</div>' +
         '</Div>');
     }
 
