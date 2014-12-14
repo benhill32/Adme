3cf32c4bf530e9283a5eeea23745e72ce5860f44
Ben Hill <ben@neocom.co.nz>
2014-12-15 12:48:58 +1300

    1

--- www/js/daily.js
+++ www/js/daily.js
@@ -48,27 +48,7 @@ function getdata_success(tx, results) {
 
         var countdown = document.getElementById("countdown");
 
-        setInterval(function () {
 
-            // find the amount of "seconds" between now and target
-            var current_date = new Date().getTime();
-            var seconds_left = (target_date - current_date) / 1000;
-
-            // do some time calculations
-            days = parseInt(seconds_left / 86400);
-            seconds_left = seconds_left % 86400;
-
-            hours = parseInt(seconds_left / 3600);
-            seconds_left = seconds_left % 3600;
-
-            minutes = parseInt(seconds_left / 60);
-            seconds = parseInt(seconds_left % 60);
-
-            // format countdown string + set tag value
-            countdown.innerHTML = hours + "h, "
-            + minutes + "m, " + seconds + "s";
-
-        }, 1000);
 
 
         var imgg = "";
@@ -92,6 +72,31 @@ function getdata_success(tx, results) {
         '<div align="center"  class="floatleft3"  >' + imgg + '</div>' +
         '<div align="center"  class="floatleft3 padding55"  >Read more</div>' +
         '</Div>');
+
+
+        setInterval(function () {
+
+            // find the amount of "seconds" between now and target
+            var current_date = new Date().getTime();
+            var seconds_left = (target_date - current_date) / 1000;
+
+            // do some time calculations
+            days = parseInt(seconds_left / 86400);
+            seconds_left = seconds_left % 86400;
+
+            hours = parseInt(seconds_left / 3600);
+            seconds_left = seconds_left % 3600;
+
+            minutes = parseInt(seconds_left / 60);
+            seconds = parseInt(seconds_left % 60);
+
+            // format countdown string + set tag value
+            countdown.innerHTML = hours + "h, "
+            + minutes + "m, " + seconds + "s";
+
+        }, 1000);
+
+
     }
 
 
