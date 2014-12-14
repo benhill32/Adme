7f6123d711f3a411db0ca3522982f1ee7086c12e
Ben Hill <ben@neocom.co.nz>
2014-12-15 12:45:35 +1300

    1

--- www/js/daily.js
+++ www/js/daily.js
@@ -47,7 +47,29 @@ function getdata_success(tx, results) {
         var target_date = new Date(day + "/" + month[month2] + "/" + year + " " + h).getTime();
 
         var countdown = document.getElementById("countdown");
-        alert(target_date);
+
+        setInterval(function () {
+
+            // find the amount of "seconds" between now and target
+            var current_date = new Date().getTime();
+            var seconds_left = (target_date - current_date) / 1000;
+
+            // do some time calculations
+            days = parseInt(seconds_left / 86400);
+            seconds_left = seconds_left % 86400;
+
+            hours = parseInt(seconds_left / 3600);
+            seconds_left = seconds_left % 3600;
+
+            minutes = parseInt(seconds_left / 60);
+            seconds = parseInt(seconds_left % 60);
+
+            // format countdown string + set tag value
+            countdown.innerHTML = hours + "h, "
+            + minutes + "m, " + seconds + "s";
+
+        }, 1000);
+
 
         var imgg = "";
         if(menu.Icon != "null"){
@@ -71,5 +93,9 @@ function getdata_success(tx, results) {
         '<div align="center"  class="floatleft3 padding55"  >Read more</div>' +
         '</Div>');
     }
+
+
+
+
 
 }
