20e2bc256c1529be04c01725ef3cafe67891900c
Ben Hill <ben@neocom.co.nz>
2014-12-16 09:17:07 +1300

    1

--- www/js/daily.js
+++ www/js/daily.js
@@ -11,7 +11,7 @@ function getdata(tx) {
     var year = current_date.getFullYear();
     var month = current_date.getMonth()+1;
     var day = current_date.getDate();
-    var hours = current_date.getHours()+1;
+    var hours = current_date.getHours();
     var mins = current_date.getMinutes();
 
     var sql = "select MAD.ID as ID,MAD.StartDate as StartDate ,MAD.EndDate as EndDate ,MAD.ItemName as ItemName,MAD.Details as Details ,MAD.Price as Price ,MAD.URL as URL, MBN.Icon as Icon,MAD.DeletedateUTC as DeletedateUTC from MobilevwApp_dailydeal as MAD JOIN MobileApp_BusinessNames as MBN on MAD.BusinessID = MBN.ID WHERE  datetime(MAD.EndDate) >=  datetime('" + year + "-" + month + "-" + day + " " + hours + ":" + mins + ":00') and MAD.DeletedateUTC = 'null' and MBN.DeletedateUTC = 'null' ";
