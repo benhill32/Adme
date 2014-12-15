1b2e99d51c2031e92c4e99f2a19125c454405185
Ben Hill <ben@neocom.co.nz>
2014-12-15 15:13:32 +1300

    1

--- www/js/daily.js
+++ www/js/daily.js
@@ -94,21 +94,8 @@ function setintervaldaily(detailarray){
         alert(item);
         var res = (item).split("|");
 
-        var seconds_left = (res[1] - current_date) / 1000;
 
-        // do some time calculations
-        //days = parseInt(seconds_left / 86400);
-        //seconds_left = seconds_left % 86400;
 
-        hours = parseInt(seconds_left / 3600);
-        seconds_left = seconds_left % 3600;
-
-        minutes = parseInt(seconds_left / 60);
-        seconds = parseInt(seconds_left % 60);
-
-        // format countdown string + set tag value
-        countdown.innerHTML = hours + "h, "
-        + minutes + "m, " + seconds + "s";
 
     }
 
