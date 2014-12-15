2ccdab93eddd9cd3ae264c95b69e3d4114402027
Ben Hill <ben@neocom.co.nz>
2014-12-15 13:54:59 +1300

    1

--- www/js/daily.js
+++ www/js/daily.js
@@ -76,7 +76,27 @@ function getdata_success(tx, results) {
 
         var countdown = document.getElementById(name);
 
-    alert(countdown.innerHTML);
+        setInterval(function () {
+
+            // find the amount of "seconds" between now and target
+            var current_date = new Date().getTime();
+            var seconds_left = (target_date - current_date) / 1000;
+
+            // do some time calculations
+            days = parseInt(seconds_left / 86400);
+            seconds_left = seconds_left % 86400;
+
+            hours = parseInt(seconds_left / 3600);
+            seconds_left = seconds_left % 3600;
+
+            minutes = parseInt(seconds_left / 60);
+            seconds = parseInt(seconds_left % 60);
+
+            // format countdown string + set tag value
+            countdown.innerHTML = hours + "h, "
+            + minutes + "m, " + seconds + "s";
+
+        }, 1000);
 
 
 
