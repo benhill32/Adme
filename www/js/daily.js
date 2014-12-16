89fd07aa80930b095e3f0ecf42bb45667441c3c4
Ben Hill <ben@neocom.co.nz>
2014-12-16 13:33:16 +1300

    1

--- www/js/daily.js
+++ www/js/daily.js
@@ -1,10 +1,38 @@
 var db;
-
+var townID = "";
 document.addEventListener("deviceready", onDeviceReadydaily, false);
 
 function onDeviceReadydaily() {
+
+    db.transaction(gettownname, errorCBfunc, successCBfunc);
+
+
+
+
+
+
+
+}
+
+function gettownname(tx) {
+    var sql = "select ID from MobileApp_Towns where Follow=1";
+    // alert(sql);
+    tx.executeSql(sql, [], gettownname_success);
+}
+
+function gettownname_success(tx, results) {
+    // $('#busy').hide();
+    var len = results.rows.length;
+    var menu = results.rows.item(0);
+    townID = menu.ID;
+
+
     db.transaction(getdata, errorCBfunc, successCBfunc);
 }
+
+
+
+
 db.transaction(getdata, errorCBfunc, successCBfunc);
 
 function getdata(tx) {
@@ -15,7 +43,7 @@ function getdata(tx) {
     var hours = ("0" + current_date.getHours()).slice(-2);
     var mins = ("0" + current_date.getMinutes()).slice(-2);
 
-    var sql = "select MAD.ID as ID,MAD.StartDate as StartDate ,MAD.EndDate as EndDate ,MAD.ItemName as ItemName,MAD.Details as Details ,MAD.Price as Price ,MAD.URL as URL, MBN.Icon as Icon,MAD.DeletedateUTC as DeletedateUTC, MBC.Follow as Follow " +
+    var sql = "select MAD.ID as ID,MAD.StartDate as StartDate ,MAD.EndDate as EndDate ,MAD.ItemName as ItemName,MAD.Details as Details ,MAD.Price as Price ,MAD.URL as URL, MBN.Icon as Icon,MAD.DeletedateUTC as DeletedateUTC, MBC.Follow as Follow, MAD.RegionID as RegionID,MAD.TownID as TownID " +
         "from MobilevwApp_dailydeal as MAD JOIN MobileApp_BusinessNames as MBN on MAD.BusinessID = MBN.ID " +
         "JOIN MobileApp_BusinessCategories as MBC on MAD.Categories = MBC.CategoryID AND MAD.BusinessID = MBC.BusniessID " +
         "WHERE MBC.Follow =1 and datetime(MAD.EndDate) >=  datetime('" + year + "-" + month + "-" + day + " " + hours + ":" + mins + ":00') and MAD.DeletedateUTC = 'null' and MBN.DeletedateUTC = 'null' ";
@@ -45,50 +73,54 @@ function getdata_success(tx, results) {
     var intervalArr = new Array();
     for (var i=0; i<len; i++) {
         var menu = results.rows.item(i);
-        var res = (menu.EndDate).split("T");
-        var split = res[0].split("-");
-        var month2 = split[1];
-        var year2 = split[0];
-        var day2 = split[2];
-        var h = res[1];
-        var name = "countdown" + menu.ID;
 
-       var target_date = new Date(day2 + "/" + month[month2] + "/" + year2 + " " + h).getTime();
 
-        var imgg = "";
-        if(menu.Icon != "null"){
-            imgg = '&nbsp;<img src="data:image/png;base64,' + menu.Icon + '" style="width:100%;"  align="center"  >&nbsp;';
-            //imgg = menu.BusinessName;
-        }else{
+        if (menu.TownID == "0" || menu.TownID == townID) {
 
-            imgg = menu.BusinessName;
+
+            var res = (menu.EndDate).split("T");
+            var split = res[0].split("-");
+            var month2 = split[1];
+            var year2 = split[0];
+            var day2 = split[2];
+            var h = res[1];
+            var name = "countdown" + menu.ID;
+
+            var target_date = new Date(day2 + "/" + month[month2] + "/" + year2 + " " + h).getTime();
+
+            var imgg = "";
+            if (menu.Icon != "null") {
+                imgg = '&nbsp;<img src="data:image/png;base64,' + menu.Icon + '" style="width:100%;"  align="center"  >&nbsp;';
+                //imgg = menu.BusinessName;
+            } else {
+
+                imgg = menu.BusinessName;
+            }
+
+            $('#dailydealsDiv').append('<Div align="center"  class=" dailydealsdiv"    >' +
+            '<div align="center" class="floatleft3remix1 padding22 paddingtop25"  >' +
+            '' +
+            'Closes' +
+            '<div id="timediv" align="center"><span id="' + name + '">' +
+            '</span></div>' +
+            '</div>' +
+            '<div align="center"  class="floatleft3remix2"  >' + imgg + '</div>' +
+            '<div align="center"  class="floatleft3remix1 padding22 paddingtop35"  >Read more</div>' +
+            '<div align="center" class="dailydivbottom"  >' + menu.Price + " " + menu.ItemName +
+            '</div>' +
+            '</Div>');
+
+            var days, hours, minutes, seconds;
+
+
+            var countdown = document.getElementById(name);
+
+
+            intervalArr.push(name + "|" + target_date);
+
+
         }
-
-        $('#dailydealsDiv').append('<Div align="center"  class=" dailydealsdiv"    >' +
-        '<div align="center" class="floatleft3remix1 padding22 paddingtop25"  >' +
-        '' +
-        'Closes' +
-        '<div id="timediv" align="center"><span id="' + name + '">' +
-        '</span></div>' +
-        '</div>' +
-        '<div align="center"  class="floatleft3remix2"  >' + imgg + '</div>' +
-        '<div align="center"  class="floatleft3remix1 padding22 paddingtop35"  >Read more</div>' +
-        '<div align="center" class="dailydivbottom"  >' + menu.Price + " " + menu.ItemName +
-        '</div>' +
-        '</Div>');
-
-        var days, hours, minutes, seconds;
-
-
-       var countdown = document.getElementById(name);
-
-
-        intervalArr.push(name + "|" + target_date);
-
-
-
     }
-
 
    setintervaldaily(intervalArr);
 }
