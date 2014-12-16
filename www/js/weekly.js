de2164b82b9782f610ca3e108624e020ec183b79
Ben Hill <ben@neocom.co.nz>
2014-12-16 15:28:12 +1300

    1

--- www/js/weekly.js
+++ www/js/weekly.js
@@ -183,7 +183,7 @@ function loadweeklyinfo(tx) {
 
     var sql = "select MAD.ID as ID,MAD.StartDate as StartDate ,MAD.EndDate as EndDate ,MAD.ItemName as ItemName,MAD.Details as Details ,MAD.Price as Price ,MAD.URL as URL, MBN.Icon as Icon,MAD.DeletedateUTC as DeletedateUTC,  MAD.RegionID as RegionID,MAD.TownID as TownID " +
         "from MobilevwApp_weeklydeal as MAD JOIN MobileApp_BusinessNames as MBN on MAD.BusinessID = MBN.ID " +
-        "WHERE MAD.ID = " + IDdaily;
+        "WHERE MAD.ID = " + IDweekly;
     // alert(sql);
     tx.executeSql(sql, [], loadweeklyinfo_success);
 
