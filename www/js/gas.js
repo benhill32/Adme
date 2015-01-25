5622f3b876293cc6d2e8c2a783901d2ba016a4d1
Ben Hill <ben@neocom.co.nz>
2015-01-26 09:00:26 +1300

    1

--- www/js/gas.js
+++ www/js/gas.js
@@ -30,11 +30,15 @@ function gettownname_success(tx, results) {
     db.transaction(getdata, errorCBfunc, successCBfunc);
 }
 
+
+
+
+
 function getdata(tx) {
 
 
 
-    var sql ="Select MGP.BusinessID,MBN.Icon as Icon ,MIN(MGP.Price91) as Price91,MIN(MGP.Price96) as Price96 ,MIN(MGP.PriceDiesel) as PriceDiesel ,MIN(MGP.PriceLPG) as PriceLPG" +
+    var sql ="Select MGP.BusinessID,MBN.Icon as Icon,MAX(MBC.Follow) as Follow ,MIN(MGP.Price91) as Price91,MIN(MGP.Price96) as Price96 ,MIN(MGP.PriceDiesel) as PriceDiesel ,MIN(MGP.PriceLPG) as PriceLPG" +
         " from MobilevwApp_GasPrices as MGP JOIN MobileApp_BusinessNames as MBN on MGP.BusinessID = MBN.ID " +
         " JOIN MobileApp_BusinessCategories as MBC on MGP.Categories = MBC.CategoryID AND MGP.BusinessID = MBC.BusniessID " +
         " where MGP.TownID =" + townID +
