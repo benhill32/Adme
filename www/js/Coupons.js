f303700948482b489f9ba5e2c9e00cd10270a647
Ben Hill <ben@neocom.co.nz>
2015-01-26 14:59:41 +1300

    1

--- www/js/Coupons.js
+++ www/js/Coupons.js
@@ -32,9 +32,9 @@ function gettownname_success(tx, results) {
 function getdata(tx) {
     var sql ="Select MGP.ID ,MGP.CreatedateUTC ,MGP.UpdatedateUTC ,MGP.DeletedateUTC ,MGP.BusinessID ,MGP.BusinessLocationID,MGP.FileName,MBN.Icon as Icon" +
         " from MobilevwApp_Coupons as MGP JOIN MobileApp_BusinessNames as MBN on MGP.BusinessID = MBN.ID " +
-        " JOIN MobileApp_BusinessCategories as MBC on MGP.Categories = MBC.CategoryID AND MGP.BusinessID = MBC.BusniessID " +
-        " where MGP.TownID =" + townID +  " and MBC.Follow =1 and MGP.DeletedateUTC = 'null' "+
-        " Order by MBN.BusinessName";
+        " JOIN MobileApp_BusinessCategories as MBC on MGP.Categories = MBC.CategoryID AND MGP.BusinessID = MBC.BusniessID ";
+        //" where MGP.TownID =" + townID +  " and MBC.Follow =1";
+
 
     alert(sql);
     tx.executeSql(sql, [], getdata_success);
