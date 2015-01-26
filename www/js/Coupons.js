c43ada637faaa206487504019fe40963a4b1f935
Ben Hill <ben@neocom.co.nz>
2015-01-26 15:07:40 +1300

    1

--- www/js/Coupons.js
+++ www/js/Coupons.js
@@ -32,7 +32,7 @@ function gettownname_success(tx, results) {
 function getdata(tx) {
     var sql ="Select MGP.ID ,MGP.CreatedateUTC ,MGP.UpdatedateUTC ,MGP.DeletedateUTC ,MGP.BusinessID ,MGP.BusinessLocationID,MGP.FileName,MBN.Icon as Icon" +
         " from MobilevwApp_Coupons as MGP JOIN MobileApp_BusinessNames as MBN on MGP.BusinessID = MBN.ID " +
-        " JOIN MobileApp_BusinessCategories as MBC on MGP.Categories = MBC.CategoryID AND MGP.BusinessID = MBC.BusniessID ";
+        " JOIN MobileApp_BusinessCategories as MBC on MGP.Categories = MBC.CategoryID AND MGP.BusinessID = MBC.BusniessID "+
         " where MGP.TownID =" + townID +  " and MBC.Follow =1";
 
 
@@ -51,10 +51,6 @@ function getdata_success(tx, results) {
         var menu = results.rows.item(i);
 
 
-
-
-
-
             var imgg = "";
             if (menu.Icon != "null") {
                 imgg = '&nbsp;<img src="data:image/png;base64,' + menu.Icon + '" style="width:50px;"  align="center"  >&nbsp;';
@@ -63,7 +59,7 @@ function getdata_success(tx, results) {
                 imgg = "";
             }
 //data-toggle="modal" data-target="#basicmodaldaily"
-            $('#gasdealsdivbody').append('<Div align="center"  class="gasdealsdiv" >' +
+            $('#coupondealsdiv').append('<Div align="center"  class="gasdealsdiv" >' +
             '<div align="center"  class="gas4sMain"   >' + imgg + '</div>' +
 
             '</Div>');
