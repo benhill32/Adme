e1ab172f6bf1582d60b13ce4f988364fe8f04577
Ben Hill <ben@neocom.co.nz>
2015-02-04 10:12:47 +1300

    1

--- www/js/Listcatagories.js
+++ www/js/Listcatagories.js
@@ -1,5 +1,5 @@
 document.addEventListener("deviceready", onDeviceReadylist, false);
-
+var regionIDlogin = 0;
 
 function onDeviceReadylist() {
     refreshdata();
@@ -10,25 +10,60 @@ function onDeviceReadylist() {
 
 function getregionslogin(tx) {
     var sql = "select ID ,CreatedateUTC,UpdatedateUTC,DeletedateUTC ,RegionName from MobileApp_Region order by RegionName ";
-     alert(sql);
+   //  alert(sql);
     tx.executeSql(sql, [], getregionslogin_success);
 }
 
 function getregionslogin_success(tx, results) {
     // $('#busy').hide();
     var len = results.rows.length;
-alert(len);
+//alert(len);
     $('#divregionnames').empty();
     for (var i=0; i<len; i++) {
         var menu = results.rows.item(i);
         var imgg = "";
 
-        $('#divregionnames').append('<Div class="modal-body" align="left" style="border-bottom: 1px solid #e5e5e5;">' +
+        $('#divregionnames').append('<Div class="modal-body" align="left" style="border-bottom: 1px solid #e5e5e5;" onclick="loadtownslogin(' + menu.ID + ')">' +
         '<div class="bold size13">' + menu.RegionName  +
         '</div>' +
         '</Div>');
     }
 }
+
+
+
+function loadtownslogin(ID) {
+    db.transaction(gettownlogin2, errorCBfunc, successCBfunc);
+    regionIDlogin = ID;
+}
+
+function gettownlogin2(tx) {
+
+    var sql = "select ID,CreatedateUTC,UpdatedateUTC,DeletedateUTC ,TownName,RegionID from MobileApp_Towns where RegionID = " + regionIDlogin;
+    alert(sql);
+    tx.executeSql(sql, [], gettownlogin2_success);
+}
+
+function gettownlogin2_success(tx, results) {
+    // $('#busy').hide();
+    var len = results.rows.length;
+//alert(len);
+    $('#divtownnames').empty();
+    for (var i=0; i<len; i++) {
+        var menu = results.rows.item(i);
+        var imgg = "";
+
+
+        $('#divtownnames').append('<Div class="modal-body"  data-dismiss="modal" align="left" style="border-bottom: 1px solid #e5e5e5;"  >' +
+        '<div class="bold size13"   >' + menu.TownName  +
+        '</div>' +
+        '</Div>');
+    }
+
+}
+
+
+
 
 
 function adddetails(){
