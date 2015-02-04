1efae1713b12a7ea730ed7947f8a991d2fb81570
Ben Hill <ben@neocom.co.nz>
2015-02-04 13:56:48 +1300

    1

--- www/js/categorieslist.js
+++ www/js/categorieslist.js
@@ -1,8 +1,43 @@
+var db;
 document.addEventListener("deviceready", onDeviceReadylistcat, false);
-var regionIDlogin = 0;
-var townIDLogin = 0;
+var catid =0;
 function onDeviceReadylistcat() {
 
     refreshdata();
+
+}
+
+
+
+
+function getcategorieslist(tx) {
+    var sql = "select ID ,CreatedateUTC,UpdatedateUTC,DeletedateUTC,CategoryName from MobileApp_Categories order by CategoryName ";
+    //alert(sql);
+    tx.executeSql(sql, [], getcategorieslist_success);
+}
+
+function getcategorieslist_success(tx, results) {
+    // $('#busy').hide();
+    var len = results.rows.length;
+//alert(len);
+    $('#catlistdiv').empty();
+    for (var i=0; i<len; i++) {
+        var menu = results.rows.item(i);
+        var imgg = "";
+
+
+        $('#catlistdiv').append('<Div class="modal-body" align="left" style="border-bottom: 1px solid #e5e5e5;" onclick="choosecatelist('+ menu.ID + ')" >' +
+        '<div class="bold size13"   >' + menu.CategoryName  +
+        '</div>' +
+        '</Div>');
+    }
+
+}
+
+function choosecatelist(ID){
+
+    catid =ID;
+  //  db.transaction(getcatname, errorCBfunc, successCBfunc);
+  //  db.transaction(getbusiness, errorCBfunc, successCBfunc);
 
 }
--- www/js/loginpage.js
+++ www/js/loginpage.js
@@ -144,12 +144,7 @@ function nextbuttonclick(){
     var DOB = $('#drpday').val() + "-" + $('#drpmonth').val() + "-" + $('#drpyear').val();
     var email = $('#txtEmail').val();
 
-
-
     passscoretoserverlogin("regionid=" + regionIDlogin + "&townid=" + townIDLogin + "&name=" + Name + "&dob=" + DOB + "&email=" + email + "&deviceid=" + deviceIDlogin + "&token=" + apptokenlogin);
-
-
-
 
 }
 
