eaea520289fc13e11d4c31f108b3ec45040bd3b5
Ben Hill <ben@neocom.co.nz>
2015-02-04 13:19:32 +1300

    1

--- www/js/functions.js
+++ www/js/functions.js
@@ -404,8 +404,8 @@ function passscoretoserver(testvar){
         var params = "?" + testvar;
 
         http.open("POST", url + params, true);
-        console.log(url + params);
-        alert(url + params);
+       // console.log(url + params);
+        //alert(url + params);
         http.onreadystatechange = function () {//Call a function when the state changes.
             if (http.readyState == 4 && http.status == 200) {
                  //alert(http.responseText);
