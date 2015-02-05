5dc90e20610fd9b47bd756ad70e93b8a635443b4
Ben Hill <ben@neocom.co.nz>
2015-02-05 14:07:12 +1300

    1

--- www/js/Catalogues.js
+++ www/js/Catalogues.js
@@ -33,7 +33,7 @@ function getdata(tx) {
     var sql ="Select MGP.ID ,MGP.CreatedateUTC ,MGP.UpdatedateUTC ,MGP.DeletedateUTC ,MGP.BusinessID ,MGP.BusinessLocationID as BusinessLocationID,MGP.FileName as FileName,MBN.Icon as Icon" +
         " from MobilevwApp_Catalogues as MGP JOIN MobileApp_BusinessNames as MBN on MGP.BusinessID = MBN.ID " +
         " JOIN MobileApp_BusinessCategories as MBC on MGP.Categories = MBC.CategoryID AND MGP.BusinessID = MBC.BusniessID "+
-        " where MGP.TownID =" + townID +  " ORDER BY MBC.Follow =1 DESC,MBN.BusinessName";
+        " where MGP.TownID =" + townID +  " ORDER BY MBC.Follow DESC,MBN.BusinessName";
 
 
   //  alert(sql);
