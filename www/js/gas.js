f7a3a237725245948235f5d5c7ad6130a14bd3e3
Ben Hill <ben@neocom.co.nz>
2015-03-02 11:09:38 +1300

    1

--- www/js/gas.js
+++ www/js/gas.js
@@ -40,7 +40,9 @@ function getdata(tx) {
     var hours = ("0" + current_date.getHours()).slice(-2);
     var mins = ("0" + current_date.getMinutes()).slice(-2);
 
-    var sql ="Select MGP.ID as ID,MBL.Address as Address,MGP.BusinessID,MBN.Icon as Icon,MBC.Follow as Follow ,MGP.Price91 as Price91,MGP.Price96 as Price96 ,MGP.PriceDiesel as PriceDiesel ,MGP.PriceLPG as PriceLPG,MGP.StartDate as StartDate,MGP.EndDate as EndDate,datetime(MGP.EndDate) as Testben " +
+    alert(('" + year + "-" + month + "-" + day + " " + hours + ":" + mins + ":00'))
+
+    var sql ="Select MGP.ID as ID,MBL.Address as Address,MGP.BusinessID,MBN.Icon as Icon,MBC.Follow as Follow ,MGP.Price91 as Price91,MGP.Price96 as Price96 ,MGP.PriceDiesel as PriceDiesel ,MGP.PriceLPG as PriceLPG,MGP.StartDate as StartDate,MGP.EndDate as EndDate,datetime(MGP.EndDate) as Testben,datetime('" + year + "-" + month + "-" + day + " " + hours + ":" + mins + ":00') as testben2 " +
         " from MobilevwApp_GasPrices as MGP JOIN MobileApp_BusinessNames as MBN on MGP.BusinessID = MBN.ID " +
         " JOIN MobileApp_BusinessCategories as MBC on MGP.Categories = MBC.CategoryID AND MGP.BusinessID = MBC.BusniessID " +
         " JOIN MobileApp_BusinessLocations as MBL on MGP.BusinessLocationID = MBL.ID" +
@@ -49,7 +51,7 @@ function getdata(tx) {
 
   //  var sql ="Select MGP.ID as ID,MGP.BusinessID ,MGP.Price91 as Price91,MGP.Price96 as Price96 ,MGP.PriceDiesel as PriceDiesel ,MGP.PriceLPG as PriceLPG,MGP.StartDate as StartDate,MGP.EndDate as EndDate " +
   //      " from MobilevwApp_GasPrices as MGP " +
-  //      " where MGP.TownID =" + townID + " and MGP.DeletedateUTC = 'null' and datetime(MGP.EndDate) >=  datetime('" + year + "-" + month + "-" + day + "T" + hours + ":" + mins + ":00')  +
+  //      " where MGP.TownID =" + townID + " and MGP.DeletedateUTC = 'null' and datetime(MGP.EndDate) >=  datetime('" + year + "-" + month + "-" + day + " " + hours + ":" + mins + ":00')  +
   //      "  ORDER BY MGP.EndDate";
 
     alert(sql);
@@ -70,7 +72,7 @@ function getdata_success(tx, results) {
 
 
         var menu = results.rows.item(i);
-        alert((menu.Testben));
+        alert((menu.Testben = " - " + menu.testben2));
         var res = (menu.EndDate).split("T");
         var split = res[0].split("-");
         var month2 = split[1];
