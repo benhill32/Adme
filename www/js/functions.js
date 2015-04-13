5bc47b921bec3ee87ce4f64ffd539b6c1850d894
Ben Hill <ben@neocom.co.nz>
2015-04-13 13:04:14 +1200

    1

--- www/js/functions.js
+++ www/js/functions.js
@@ -9,7 +9,7 @@ var databaseversion;
 var appversion = -1;
 var apptoken = 0;
 var networkconnectionfun= 0;
-var appversionlocal = '1.3.2';
+var appversionlocal = '1.3.3';
 
 
 function onDeviceReadyFunc() {
