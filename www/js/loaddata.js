12525a4cb14e87e5e76d2f7f6ff3b9eaed205d3f
benhill32 <ben@neocom.co.nz>
2015-07-24 13:31:08 +1200

    1
    1

--- www/js/loaddata.js
+++ www/js/loaddata.js
@@ -310,8 +310,8 @@ function closemodelRegion(){
   //  db.transaction(gettokenlogin, errorCBfunc, successCBfunc);
   //
 
-    db.transaction(getregiontownlogin1, errorCBfunc, successCBfunc);
-  //  db.transaction(getregionslogin, errorCBfunc, successCBfunc);
+   // db.transaction(getregiontownlogin1, errorCBfunc, successCBfunc);
+    db.transaction(getregionslogin, errorCBfunc, successCBfunc);
    // randomfunctions();
 
 }
